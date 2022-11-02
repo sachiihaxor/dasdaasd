669219be47d66e42f1ef0fd9ba84104a19b799fb
sachiihaxor <106025611+sachiihaxor@users.noreply.github.com>
2022-11-02 15:37:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>AFEBI Journal System</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="generator" content="Open Journal Systems 2.4.6.0" />
+	
+		<link rel="stylesheet" href="https://journal.afebi.org/lib/pkp/styles/pkp.css" type="text/css" />
+	<link rel="stylesheet" href="https://journal.afebi.org/lib/pkp/styles/common.css" type="text/css" />
+	<link rel="stylesheet" href="https://journal.afebi.org/styles/common.css" type="text/css" />
+	<link rel="stylesheet" href="https://journal.afebi.org/styles/compiled.css" type="text/css" />
+
+	<!-- Base Jquery -->
+	<script type="text/javascript" src="//www.google.com/jsapi"></script>
+		<script type="text/javascript">
+			<!--
+			// Provide a local fallback if the CDN cannot be reached
+			if (typeof google == 'undefined') {
+				document.write(unescape("%3Cscript src='https://journal.afebi.org/lib/pkp/js/lib/jquery/jquery.min.js' type='text/javascript'%3E%3C/script%3E"));
+				document.write(unescape("%3Cscript src='https://journal.afebi.org/lib/pkp/js/lib/jquery/plugins/jqueryUi.min.js' type='text/javascript'%3E%3C/script%3E"));
+			} else {
+				google.load("jquery", "1.4.4");
+				google.load("jqueryui", "1.8.6");
+			}
+			// -->
+		</script>
+	
+	
+	
+	<link rel="stylesheet" href="https://journal.afebi.org/styles/sidebar.css" type="text/css" />		<link rel="stylesheet" href="https://journal.afebi.org/styles/rightSidebar.css" type="text/css" />	
+			<link rel="stylesheet" href="https://journal.afebi.org/public/site/sitestyle.css" type="text/css" />
+			<link rel="stylesheet" href="https://journal.afebi.org/plugins/blocks/languageToggle/styles/languageToggle.css" type="text/css" />
+	
+	<!-- Default global locale keys for JavaScript -->
+	
+<script type="text/javascript">
+	jQuery.pkp = jQuery.pkp || { };
+	jQuery.pkp.locale = { };
+			
+				jQuery.pkp.locale.form_dataHasChanged = 'The data on this form has changed. Continue anyway?';
+	</script>
+	<!-- Compiled scripts -->
+			
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/lib/jquery/plugins/jquery.tag-it.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/lib/jquery/plugins/jquery.cookie.js"></script>
+
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/functions/fontController.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/functions/general.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/functions/jqueryValidatorI18n.js"></script>
+
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/classes/Helper.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/classes/ObjectProxy.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/classes/Handler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/classes/linkAction/LinkActionRequest.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/classes/features/Feature.js"></script>
+
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/SiteHandler.js"></script><!-- Included only for namespace definition -->
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/UrlInDivHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/AutocompleteHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/ExtrasOnDemandHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/form/FormHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/form/AjaxFormHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/form/ClientFormHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/grid/GridHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/controllers/linkAction/LinkActionHandler.js"></script>
+
+<script type="text/javascript" src="https://journal.afebi.org/js/pages/search/SearchFormHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/js/statistics/ReportGeneratorFormHandler.js"></script>
+<script type="text/javascript" src="https://journal.afebi.org/plugins/generic/lucene/js/LuceneAutocompleteHandler.js"></script>
+
+<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/lib/jquery/plugins/jquery.pkp.js"></script>	
+	<!-- Form validation -->
+	<script type="text/javascript" src="https://journal.afebi.org/lib/pkp/js/lib/jquery/plugins/validate/jquery.validate.js"></script>
+	<script type="text/javascript">
+		<!--
+		// initialise plugins
+		
+		$(function(){
+			jqueryValidatorI18n("https://journal.afebi.org", "en_US"); // include the appropriate validation localization
+			
+			$(".tagit").live('click', function() {
+				$(this).find('input').focus();
+			});
+		});
+		// -->
+		
+	</script>
+
+		
+</head>
+<body id="pkp-common-openJournalSystems">
+<div id="container">
+
+<div id="header">
+<div id="headerTitle">
+<h1>
+	<img src="https://journal.afebi.org/public/site/pageHeaderTitleImage_en_US.jpg" width="1020" height="154" alt="AFEBI Journal System" />
+</h1>
+</div>
+</div>
+
+<div id="body">
+
+	<div id="sidebar">
+							<div id="rightSidebar">
+				<div class="block" id="sidebarDevelopedBy">
+	<a class="blockTitle" href="http://pkp.sfu.ca/ojs/" id="developedBy">Open Journal Systems</a>
+</div><div class="block" id="sidebarHelp">
+	<a class="blockTitle" href="javascript:openHelp('https://journal.afebi.org/index.php/index/help/view/user/topic/000001')">Journal Help</a>
+</div><div class="block" id="sidebarUser">
+			<span class="blockTitle">User</span>
+	
+						<form method="post" action="https://journal.afebi.org/index.php/index/login/signIn">
+				<table>
+					<tr>
+						<td><label for="sidebar-username">Username</label></td>
+						<td><input type="text" id="sidebar-username" name="username" value="" size="12" maxlength="32" class="textField" /></td>
+					</tr>
+					<tr>
+						<td><label for="sidebar-password">Password</label></td>
+						<td><input type="password" id="sidebar-password" name="password" value="" size="12" class="textField" /></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type="checkbox" id="remember" name="remember" value="1" /> <label for="remember">Remember me</label></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type="submit" value="Login" class="button" /></td>
+					</tr>
+				</table>
+			</form>
+			</div> 
+<div class="block" id="sidebarLanguageToggle">
+	<script type="text/javascript">
+		<!--
+		function changeLanguage() {
+			var e = document.getElementById('languageSelect');
+			var new_locale = e.options[e.selectedIndex].value;
+
+			var redirect_url = 'https://journal.afebi.org/index.php/index/user/setLocale/NEW_LOCALE?source=%2F';
+			redirect_url = redirect_url.replace("NEW_LOCALE", new_locale);
+
+			window.location.href = redirect_url;
+		}
+		//-->
+	</script>
+	<span class="blockTitle">Language</span>
+	<form action="#">
+		<label for="languageSelect">Select Language</label>
+		<select id="languageSelect" size="1" name="locale" class="selectMenu"><option label="English" value="en_US" selected="selected">English</option>
+<option label="Indonesian" value="id_ID">Indonesian</option>
+</select>
+		<input type="submit" class="button" value="Submit" onclick="changeLanguage(); return false;" />
+	</form>
+</div>
+<div class="block" id="sidebarNavigation">
+	<span class="blockTitle">Journal Content</span>
+
+	<form id="simpleSearchForm" method="post" action="https://journal.afebi.org/index.php/index/search/search">
+		<table id="simpleSearchInput">
+			<tr>
+				<td>
+													<label for="simpleQuery">Search <br />
+					<input type="text" id="simpleQuery" name="simpleQuery" size="15" maxlength="255" value="" class="textField" /></label>
+								</td>
+			</tr>
+			<tr>
+				<td><label for="searchField">
+				Search Scope
+				<br />
+				<select id="searchField" name="searchField" size="1" class="selectMenu">
+					<option label="All" value="query">All</option>
+<option label="Authors" value="authors">Authors</option>
+<option label="Title" value="title">Title</option>
+<option label="Abstract" value="abstract">Abstract</option>
+<option label="Index terms" value="indexTerms">Index terms</option>
+<option label="Full Text" value="galleyFullText">Full Text</option>
+
+				</select></label>
+				</td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="Search" class="button" /></td>
+			</tr>
+		</table>
+	</form>
+
+	<br />
+
+	</div>
+
+<!-- Add javascript required for font sizer -->
+<script type="text/javascript">
+	<!--
+	$(function(){
+		fontSize("#sizer", "body", 9, 16, 32, ""); // Initialize the font sizer
+	});
+	// -->
+</script>
+
+<div class="block" id="sidebarFontSize" style="margin-bottom: 4px;">
+	<span class="blockTitle">Font Size</span>
+	<div id="sizer"></div>
+</div>
+<br />
+			</div>
+			</div>
+
+<div id="main">
+<div id="navbar">
+	<ul class="menu">
+		<li id="home"><a href="https://journal.afebi.org/index.php/index/index">Home</a></li>
+		<li id="about"><a href="https://journal.afebi.org/index.php/index/about">About</a></li>
+
+					<li id="login"><a href="https://journal.afebi.org/index.php/index/login">Login</a></li>
+							<li id="register"><a href="https://journal.afebi.org/index.php/index/user/register">Register</a></li>
+												<li id="search"><a href="https://journal.afebi.org/index.php/index/search">Search</a></li>
+		
+		
+				
+
+			</ul>
+</div>
+<div id="breadcrumb">
+	<a href="https://journal.afebi.org/index.php/index/index">Home</a> &gt;
+			<a href="https://journal.afebi.org/index.php" class="current">AFEBI Journal System</a></div>
+
+<h2>AFEBI Journal System</h2>
+
+
+<div id="content">
+
+
+<br />
+
+<div id="intro"><p align="justify">Association of Faculty of Economic and Business Indonesia (AFEBI) is a publisher of scientific papers from academicians to other professionals in the field of economics and business. It covers AFEBI Islamic Finance and Economic Review, AFEBI Accounting Review, AFEBI Economic and Finance Review, and AFEBI Management and Business Review. It is published twice a year in June and December. The objective of the journal is as the dissemination of theory and research in all areas of business and management, Islamic economics, accounting and economics.</p><p align="justify"><strong>Indexed:</strong></p><p align="justify"><a href="https://scholar.google.com/scholar?hl=id&amp;q=afebi&amp;as_sdt=0" target="_blank"><strong><img src="https://journal.uii.ac.id/public/site/images/deni/GS2.png" alt="" /></strong></a><strong><a href="https://garuda.kemdikbud.go.id/publisher/view/866" target="_blank"><img src="https://journal.uii.ac.id/public/site/images/deni/garuda.png" alt="" /></a><a href="https://search.crossref.org/?from_ui=&amp;q=AFEBI" target="_blank"><img src="https://journal.uii.ac.id/public/site/images/deni/crossref2.png" alt="" /></a><a href="https://app.dimensions.ai/discover/publication?search_mode=content&amp;search_text=AFEBI&amp;search_type=kws&amp;search_field=full_search&amp;and_facet_journal=jour.1151466" target="_blank"><img src="https://journal.uii.ac.id/public/site/images/deni/Dimension1.png" alt="" /></a></strong></p></div>
+<a name="journals"></a>
+
+
+					<div style="clear:left;">
+								<div class="homepageImage"><a href="https://journal.afebi.org/index.php/ambr" class="action"><img src="https://journal.afebi.org/public/journals/1/journalThumbnail_en_US.jpg" alt="Page Header Logo" /></a></div>
+				</div>
+				<h3>AFEBI Management and Business Review</h3>
+			<p><a href="https://journal.afebi.org/index.php/ambr" class="action">View Journal</a> | <a href="https://journal.afebi.org/index.php/ambr/issue/current" class="action">Current Issue</a> | <a href="https://journal.afebi.org/index.php/ambr/user/register" class="action">Register</a></p>
+					<div style="clear:left;">
+								<div class="homepageImage"><a href="https://journal.afebi.org/index.php/aefr" class="action"><img src="https://journal.afebi.org/public/journals/2/journalThumbnail_en_US.jpg" alt="Page Header Logo" /></a></div>
+				</div>
+				<h3>AFEBI Economic and Finance Review</h3>
+			<p><a href="https://journal.afebi.org/index.php/aefr" class="action">View Journal</a> | <a href="https://journal.afebi.org/index.php/aefr/issue/current" class="action">Current Issue</a> | <a href="https://journal.afebi.org/index.php/aefr/user/register" class="action">Register</a></p>
+					<div style="clear:left;">
+								<div class="homepageImage"><a href="https://journal.afebi.org/index.php/aar" class="action"><img src="https://journal.afebi.org/public/journals/3/journalThumbnail_en_US.jpg" alt="Page Header Logo" /></a></div>
+				</div>
+				<h3>AFEBI Accounting Review</h3>
+			<p><a href="https://journal.afebi.org/index.php/aar" class="action">View Journal</a> | <a href="https://journal.afebi.org/index.php/aar/issue/current" class="action">Current Issue</a> | <a href="https://journal.afebi.org/index.php/aar/user/register" class="action">Register</a></p>
+					<div style="clear:left;">
+								<div class="homepageImage"><a href="https://journal.afebi.org/index.php/aifer" class="action"><img src="https://journal.afebi.org/public/journals/4/journalThumbnail_en_US.jpg" alt="Page Header Logo" /></a></div>
+				</div>
+				<h3>AFEBI Islamic Finance and Economic Review</h3>
+			<p><a href="https://journal.afebi.org/index.php/aifer" class="action">View Journal</a> | <a href="https://journal.afebi.org/index.php/aifer/issue/current" class="action">Current Issue</a> | <a href="https://journal.afebi.org/index.php/aifer/user/register" class="action">Register</a></p>
+
+<div id="journalListPageInfo"></div>
+<div id="journalListPageLinks"></div>
+
+
+</div><!-- content -->
+</div><!-- main -->
+</div><!-- body -->
+
+
+
+</div><!-- container -->
+</body>
+</html>
